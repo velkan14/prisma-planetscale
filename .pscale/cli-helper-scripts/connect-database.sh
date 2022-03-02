f6ca524da8884bdaeeaab5c67de4b95b9e9c1d39
danielamado <daniel@precycle.today>
2022-03-02 10:52:38 +0100

    change connect script

--- .pscale/cli-helper-scripts/connect-database.sh
+++ .pscale/cli-helper-scripts/connect-database.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-. use-pscale-docker-image.sh
 
-echo "pscale connect $DB_NAME preview --port 3306 --host 0.0.0.0"
-pscale connect "$DB_NAME" preview --port 3306 --host 0.0.0.0 --service-token $PLANETSCALE_SERVICE_TOKEN
+command="docker run -e HOME=/tmp -v $HOME/.config/planetscale:/tmp/.config/planetscale -e PSCALE_ALLOW_NONINTERACTIVE_SHELL=true -p 127.0.0.1:3309:3306 --user $(id -u):$(id -g) --rm planetscale/pscale:${PSCALE_VERSION:-"latest"} connect $DB_NAME preview --port 3306 --host 0.0.0.0 --service-token $PLANETSCALE_SERVICE_TOKEN"
+echo command
+$command
 docker ps
