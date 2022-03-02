01aea6e85705c8e63e51bcf9fbaa825db81099e8
danielamado <daniel@precycle.today>
2022-03-02 12:56:06 +0100

    check command

--- .pscale/cli-helper-scripts/connect-database.sh
+++ .pscale/cli-helper-scripts/connect-database.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-command="docker run -d -e HOME=/tmp -v $HOME/.config/planetscale:/tmp/.config/planetscale -e PSCALE_ALLOW_NONINTERACTIVE_SHELL=true -p 127.0.0.1:3309:3306 --user $(id -u):$(id -g) --rm planetscale/pscale:${PSCALE_VERSION:-"latest"} connect $DB_NAME preview --port 3306 --host 0.0.0.0 --org $ORG_NAME --service-token $PLANETSCALE_SERVICE_TOKEN --service-token-name $PLANETSCALE_SERVICE_TOKEN_NAME"
-echo $command
-$command
+docker run -d \
+-e HOME=/tmp \
+-e PSCALE_ALLOW_NONINTERACTIVE_SHELL=true \
+-v $HOME/.config/planetscale:/tmp/.config/planetscale \
+-p 127.0.0.1:3309:3306 \
+--user $(id -u):$(id -g) \
+--rm planetscale/pscale:${PSCALE_VERSION:-"latest"} \
+connect $DB_NAME preview --port 3306 --host 0.0.0.0 --org $ORG_NAME --service-token $PLANETSCALE_SERVICE_TOKEN --service-token-name $PLANETSCALE_SERVICE_TOKEN_NAME
