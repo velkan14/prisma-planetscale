dbbb9fbb34629f2a88d8b8bb93dfc78617aef633
danielamado <daniel@precycle.today>
2022-03-04 10:48:15 +0100

    checl deploy request

--- .pscale/cli-helper-scripts/create-db-branch-dr-and-connection.sh
+++ .pscale/cli-helper-scripts/create-db-branch-dr-and-connection.sh
@@ -7,4 +7,5 @@ BRANCH_NAME="$1"
 . set-db-and-org-and-branch-name.sh
 
 . ps-create-helper-functions.sh
+check-deploy-request "$DB_NAME" preview "$ORG_NAME"
 create-deploy-request "$DB_NAME" preview "$ORG_NAME"
